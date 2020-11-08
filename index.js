6349f2167ea38fcea5123f70d2a8facc85fa0c4e
hanjuren <juren528@gmail.com>
2020-11-08 11:30:06 +0900

    Babel & nodemon setting

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require('express');
+import express from "express";
 const app = express(); 
 
 const PORT = 4000;
  
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`); //서버 생성
+
+const handelHome = (req, res) => res.send("Hello from my ass");
+//babel 사용. arrow function of javascript
+const handleProfile = (req, res) => res.send("You are on my profile"); //응답.
+
+const betweenHome = (req, res, next) => {
+    console.log("between");
+    next();
+};
+
+app.use(betweenHome);
+
+app.get("/", handelHome); //라우터 생성
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
 
