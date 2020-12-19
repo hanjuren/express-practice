8efbb6813da2d3e01b4ef882c4e8ca42ab9bf22b
hanjuren <juren528@gmail.com>
2020-12-19 16:40:19 +0900

    '

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:324393,
+        title: 'Video awesome',
+        descripition: 'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name:"Han Juryeon",
+            email:"juren52@naver.com"
+        }
+    },
+    {
+        id:111111,
+        title: 'Video super',
+        descripition: 'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name:"Han Juryeon",
+            email:"juren52@naver.com"
+        }
+    },
+    {
+        id:12121,
+        title: 'Video nice',
+        descripition: 'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name:"Han Juryeon",
+            email:"juren52@naver.com"
+        }
+    },
+    {
+        id:111393,
+        title: 'Video pe',
+        descripition: 'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name:"Han Juryeon",
+            email:"juren52@naver.com"
+        }
+    }
+]
