c11f7e21fcd17255de21c4598a2de4af18594030
hanjuren <juren528@gmail.com>
2021-01-23 15:13:17 +0900

    express : middle wares prat2

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import express from "express";
-import { nextTick } from "process";
+import express from "express"; // 서버
+import morgan from "morgan"; // node.js 용 로깅 미들웨어
+import helmet from "helmet"; // 보안을 위해 사용
+import cookieParser from "cookie-parser";
+import bodyParser, { json } from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -16,12 +19,11 @@ const handleProfile = (req, res) => {
     res.send('Hello from Profile');
 }
 
-const betweenHome = (req, res, next) => { // 미들웨어는 req, res, next
-    console.log("i'm between");
-    next(); // next를 호출해야 다음 미들웨어로 넘어간다.
-}
-
-app.use(betweenHome); // 미들웨어 순서 중요...
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // 중첩된 객체 표현을 할지 말지 정하는것 true는 qs모듈사용 false는 query-string모듈 사용
+app.use(helmet());
+app.use(morgan('dev'));
 
 app.get('/', handleHome);
 
